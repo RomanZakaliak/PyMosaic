9289de6eff1760bae73aa35bd70ef53db5e6fc21
Rookie <37544154+RomanZakaliak@users.noreply.github.com>
2021-02-07 22:14:16 +0200

    added experimental replacement Flask by Quart

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import secrets
-from flask import Flask, render_template, request, redirect, make_response, send_from_directory, jsonify
+from quart import Quart, render_template, request, redirect, make_response, send_from_directory, jsonify
 from werkzeug.utils import secure_filename
 
 from src.image import ImageTransform
@@ -16,13 +16,13 @@ if not os.path.exists(UPLOAD_FOLDER):
 if not os.path.exists(RESULT_FOLDER):
     os.mkdir(RESULT_FOLDER)
 
-app = Flask(__name__, static_url_path = '')
+app = Quart(__name__, static_url_path = '')
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['SECRET_KEY'] = secrets.token_hex(30)
 
-def get_file_extension(filename):
-    return filename.rsplit('.', 1)[1].lower()
+async def get_file_extension(filename):
+    return await filename.rsplit('.', 1)[1].lower()
 
 def allowed_file(filename):
     return '.' in filename and \
@@ -37,20 +37,22 @@ def process_file(filename, chunk_size):
 
 
 @app.route('/', methods = ['GET'])
-def index(file_name:str = None):
-    return render_template('index.html.jinja')
+async def index(file_name:str = None):
+    return await render_template('index.html.jinja')
 
 @app.route('/upload_file', methods = ['POST'])
-def upload_file():
+async def upload_file():
     if request.method == 'POST':
-        if 'file' not in request.files or 'chunk_size' not in request.form:
-            return redirect(request.url)
+        req_files = await request.files
+        req_form = await request.form
+        if 'file' not in req_files or 'chunk_size' not in req_form:
+            return await redirect(request.url)
 
-        file = request.files['file']
-        chunk_size = int(request.form['chunk_size'])
+        file = req_files['file']
+        chunk_size = int(req_form['chunk_size'])
 
         if file.filename == '':
-            return redirect(request.url)
+            return await redirect(request.url)
 
         file_extension = get_file_extension(file.filename)
         file.filename = f'{secrets.token_hex(10)}.{file_extension}'
@@ -58,21 +60,21 @@ def upload_file():
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            file.save(full_filename)
+            await file.save(full_filename)
             process_file(filename, chunk_size)
             res = jsonify({'filename':str(filename)})
-            return make_response(res)
+            return await make_response(res)
 
 @app.route('/download/<string:filename>')
-def download_file(filename):
+async def download_file(filename):
     accessed_filename = os.path.join(RESULT_FOLDER, filename)
     if os.path.isfile(accessed_filename):
-        return send_from_directory(directory=RESULT_FOLDER, filename=filename)
+        return await send_from_directory(directory=RESULT_FOLDER, file_name=filename)
     else:
         raise Exception("File does not exists")
 
 @app.errorhandler(Exception)
-def handle_exception(exp):
-    return render_template('error.html.jinja', error=exp)
+async def handle_exception(exp):
+    return await render_template('error.html.jinja', error=exp)
 
 if __name__ == "__main__": app.run(debug=True, host='0.0.0.0')

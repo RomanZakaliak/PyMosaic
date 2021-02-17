ba951014d3a0ad4ebb9bc9c013133fa4434dc958
Rookie <37544154+RomanZakaliak@users.noreply.github.com>
2021-02-17 17:33:46 +0200

    added check if folder does not exist

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import os 
 import logging
 import jinja2
 import aiohttp_jinja2
 from aiohttp import web
 import asyncio
 
-from config import TEMPLATES_PATH
+from config import TEMPLATES_PATH, RESULT_FOLDER, THUMBNAILS_FOLDER
 from views import SiteHandler
 from router import setup_routes
+
+def createFolders():
+    if not os.path.exists(RESULT_FOLDER):
+        os.mkdir(RESULT_FOLDER)
+    if not os.path.exists(THUMBNAILS_FOLDER):
+        os.mkdir(THUMBNAILS_FOLDER)
 
 def setup_jinja(app):
     loader = jinja2.FileSystemLoader(str(TEMPLATES_PATH))
@@ -15,6 +22,9 @@ def setup_jinja(app):
 
 async def init_app():
     app = web.Application(client_max_size=3 * 1024 ** 2)
+    
+    createFolders()
+
     setup_jinja(app)
 
     handler = SiteHandler()
